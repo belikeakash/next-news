8609eaa47192a0b5c9dec4269d25f364f21939a6
Akash <belikeavkash@gmail.com>
2022-09-22 00:19:04 +0530

    4th commit

--- components/Navbar.js
+++ components/Navbar.js
@@ -9,7 +9,7 @@ export const Navbar = () => {
     return (
         <div className={styles.main}>
             <div><Link href="/">Home</Link></div>
-            <div><Link href="/feed/1">Feed</Link></div>
+            <div><Link href="/feed/1">News</Link></div>
             <div><Link href="/contributors">Contributors</Link></div>
             <div><Link href="https://www.linkedin.com/in/akash-sirohi-676b30202/">Linkedin</Link></div>
             <div><Link href="https://github.com/belikeakash/next-news">Github</Link></div>
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import { Navbar } from '../components/Navbar'
+import Link from 'next/link'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
@@ -11,7 +12,7 @@ export default function Home() {
       <div className={styles.main}>
         <h1>Next.js News App</h1>
         <h3>Here You find every news </h3>
-
+        <h4>Head Forward to <Link href = "/feed/1"><span className={styles.apple}> News </span></Link> </h4>
       </div>
     </div>
   )
