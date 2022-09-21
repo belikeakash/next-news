323fd5ad26b78446b236918197a88327ed06c370
Akash <belikeavkash@gmail.com>
2022-09-22 00:22:59 +0530

    5th commit

--- components/Navbar.js
+++ components/Navbar.js
@@ -11,7 +11,7 @@ export const Navbar = () => {
             <div><Link href="/">Home</Link></div>
             <div><Link href="/feed/1">News</Link></div>
             <div><Link href="/contributors">Contributors</Link></div>
-            <div><Link href="https://www.linkedin.com/in/akash-sirohi-676b30202/">Linkedin</Link></div>
+            <div><Link href="https://www.linkedin.com/in/akash-sirohi-676b30202/">LinkedIn</Link></div>
             <div><Link href="https://github.com/belikeakash/next-news">Github</Link></div>
         </div>
     )
