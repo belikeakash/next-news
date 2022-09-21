b8fb8f2d3b3b24c50984cb86c9a0602737ec6ec7
Akash <belikeavkash@gmail.com>
2022-09-21 23:21:34 +0530

    2nd commit

---
+++ components/Navbar.js
@@ -0,0 +1,19 @@
+import {useRouter} from 'next/router'
+import Link from 'next/link'
+import styles from '../styles/Navbar.module.css'
+
+export const Navbar = () => {
+
+    const router = useRouter();
+
+    return (
+        <div className={styles.main}>
+            <div><Link href="/">Home</Link></div>
+            <div><Link href="/feed/">Feed</Link></div>
+            <div><Link href="/contributors">Contributors</Link></div>
+            <div><Link href="https://www.linkedin.com/in/akash-sirohi-676b30202/">Linkedin</Link></div>
+            <div><Link href="https://github.com/belikeakash/next-news">Github</Link></div>
+        </div>
+    )
+
+}
--- pages/contributors.js
+++ pages/contributors.js
@@ -1,15 +1,36 @@
+import styles from '../styles/Contributors.module.css'
+import Link from 'next/link'
+import { Navbar } from '../components/Navbar';
 export const Contributor = ({contributors}) =>{
+
+    console.log(contributors);
     return(
         <div className="page-conatiner">
-            <div className="">
-                <h1></h1>
+            <Navbar />
+            <div className={styles.main}>
+                <h1>Contributors</h1>
+                <div className={styles.contributors}>
+                    <h3>{contributors.name}</h3>
+                    <h4>Current Comapny : <Link style={{color:"red"}} href = "https://www.tiket.com/">{contributors.currentcompany}</Link> </h4>
+                    <h6>{contributors.position}</h6>
+                    <img src={contributors.image} />
+                    <p>{contributors.description}</p>
+                </div>
             </div>
         </div>
     )
 } 
 
 export const getServerSideProps = async pageContent => {
-    const apiResponse = await fetch ()
+    const apiResponse = await fetch (
+        'https://my-json-server.typicode.com/belikeakash/next-news/contributors')
+        const contributors = await apiResponse.json();
+
+    return {
+        props : {
+            contributors : contributors
+        }
+    }
 }
 
 export default Contributor
