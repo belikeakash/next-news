3bd5f0c597b532267e50f9cab7b96c6e2e6d1d91
Akash <belikeavkash@gmail.com>
2022-09-21 21:53:51 +0530

    1st commit

---
+++ pages/contributors.js
@@ -0,0 +1,15 @@
+export const Contributor = ({contributors}) =>{
+    return(
+        <div className="page-conatiner">
+            <div className="">
+                <h1></h1>
+            </div>
+        </div>
+    )
+} 
+
+export const getServerSideProps = async pageContent => {
+    const apiResponse = await fetch ()
+}
+
+export default Contributor
